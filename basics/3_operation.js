d61f6f5499fd1b6668b57c5a040cc854da61409e
Yash Dubey <yashdubey9764@gmail.com>
2026-05-21 10:57:23 +0000

    operation

---
+++ basics/3_operation.js
@@ -0,0 +1,33 @@
+// let value =3
+// let negValue=-value
+// console.log(negValue) //-3
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3) //2 to the power of 3
+// console.log(2%2)    
+
+// let str1="hello"
+// let str2="world"
+
+// let str3=str1+str2
+// console.log(str3) //helloworld
+// console.log("1"+2) //12
+// console.log(1+"2") //12
+// console.log(1+2+"2") //32
+// console.log("1"+2+2) //122
+// console.log((1+2) * 3 % 5) //3
+console.log(+true) //1
+console.log(+"") //0
+// console.log(true+) // SyntaxError: Unary operator expected
+
+let num1,num2,num3
+num1=num2=num3=5
+console.log(num1,num2,num3) //5 5 5
+
+let gameCounter=100
+// gameCounter++
+++gameCounter
+console.log(gameCounter) //101
+gameCounter-=4
+console.log(gameCounter) //97
