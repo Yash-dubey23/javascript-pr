0e1f7895ebc285f5f3a9195c4763814453dfddcd
Yash Dubey <yashdubey9764@gmail.com>
2026-05-22 09:11:43 +0000

    comparison

---
+++ basics/4_comparison.js
@@ -0,0 +1,16 @@
+// console.log(2<5);
+// console.log(2==8);
+// console.log(6/3);
+
+// console.log(null > 0);   // false
+// console.log(null == 0);  // false
+// console.log(null >= 0);  // true
+// /* different rule for different opwerator    so you should avoid using this type of comparison
+// ==
+// null only equals undefined
+
+// > < >= <=
+// null becomes 0*/
+
+console.log("2"=== 2);  // false   // it checks type and value
+console.log("2">= 2);  // true
